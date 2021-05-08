0004326eab51210bd1380a6a9f2c4613b6f8875c
Matan Liram <matanl@protonmail.com>
2021-05-08 12:29:44 +0300

    Tried to add card to update-jewelry, still doesn't work

--- shira-studio/src/app/admin-routing.module.ts
+++ shira-studio/src/app/admin-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from './admin-panel/dashboard/dashboard.component';
+import {UpdateJewelryComponent} from './admin-panel/update-jewelry/update-jewelry.component';
 
 const routes: Routes = [
-  { path: 'admin', component: DashboardComponent},
+  { path: 'admin', children: [
+      { path: 'add', component: UpdateJewelryComponent },
+      { path: '', component: DashboardComponent }
+    ]
+  }
 ];
 
 @NgModule({
